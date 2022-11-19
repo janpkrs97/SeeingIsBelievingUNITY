7a632ad7d5980a53eed5de6466b21c962366fe67
janpkrs97 <57917997+janpkrs97@users.noreply.github.com>
2022-11-19 12:35:43 +0200

    ReferenceManager Configuration

---
+++ Assets/Scripts/ReferenceController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReferenceController : MonoBehaviour
+{
+    private GameObject managersParent;
+    public PlayerController playerController;
+    public SceneController sceneController;
+
+    void Awake()
+    {
+        managersParent = GameObject.Find("Managers");
+        playerController = managersParent.GetComponentInChildren<PlayerController>();
+        sceneController = managersParent.GetComponentInChildren<SceneController>();
+    }
+
+    public void ChangePlayerVisualization (int id)
+    {
+        if (id == 0)
+        {
+            playerController.VisualizeFullBody();
+        }
+        else if (id == 1)
+        {
+            playerController.VisualizeUpperBodyOnly();
+        }
+        else if (id == 2)
+        {
+            playerController.VisualizeFaceOnly();
+        }
+        else
+        {
+            playerController.VisualizeFullBody();
+        }
+    }
+}
