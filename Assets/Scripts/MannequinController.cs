6f436576422069a728a6f75ac5fca6817d83143b
janpkrs97 <57917997+janpkrs97@users.noreply.github.com>
2022-11-29 09:27:41 +0200

    Home Scene Surgery Stage Option Implementation

--- Assets/Scripts/MannequinController.cs
+++ Assets/Scripts/MannequinController.cs
@@ -4,9 +4,6 @@ using UnityEngine;
 
 public class MannequinController : MonoBehaviour
 {
-    public GameObject standingPatientMannequin;
-    public GameObject lyingPatientMannequin;
-
     public Material[] mannequinMaterials;
 
     public void ChangeMannequinSurgeryStage (int id)
@@ -17,10 +14,5 @@ public class MannequinController : MonoBehaviour
         {
             obj.GetComponent<SkinnedMeshRenderer>().material = mannequinMaterials[id];
         }
-        
-        //bodies[0].GetComponent<SkinnedMeshRenderer>().material = mannequinMaterials[id];
-        //bodies[1].GetComponent<SkinnedMeshRenderer>().material = mannequinMaterials[id];
-        //standingPatientMannequin.GetComponent<SkinnedMeshRenderer>().material = mannequinMaterials[id];
-        //lyingPatientMannequin.GetComponent<SkinnedMeshRenderer>().material = mannequinMaterials[id];
     }
 }
