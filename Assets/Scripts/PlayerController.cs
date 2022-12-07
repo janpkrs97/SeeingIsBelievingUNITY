2944791702f315cbf04c50256b4147a9dc8cbfaf
janpkrs97 <57917997+janpkrs97@users.noreply.github.com>
2022-12-07 11:19:34 +0200

    Lighting Improvements

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class PlayerController : MonoBehaviour
 {
